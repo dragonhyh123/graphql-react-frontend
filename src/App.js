13ce9a9a38c2c0d606a9a2d1339777a4aeb78212
youhe <youhe@ebay.com>
2021-06-14 00:06:34 +0800

    new version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useLazyQuery,gql } from '@apollo/client';
+import {useState} from 'react';
 
 function App() {
+  const GET_USER = gql`
+    query($Id:ID!){
+      user(id:$Id){
+        name
+        age
+        available
+        money
+        gender
+        birthday
+      }
+  
+      post(id:$Id){
+        title
+        content
+        user{
+          name
+    	    age
+    	    available
+    	    money
+    	    gender
+    	    birthday
+        }
+      }
+    }
+  `;
+
+  const [getData] = useLazyQuery(GET_USER,{onCompleted:(result)=>{
+    console.log(result);
+  }});
+
+  let[number,setNumber] = useState(1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button style={{marginTop:600}} onClick={(oEvent)=>{
+        getData({variables:{Id:number}});
+        setNumber(++number);
+      }}>Click me</button>
     </div>
   );
 }
